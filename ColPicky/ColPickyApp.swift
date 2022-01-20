4467fa4c4850cc6c196829101d17982489628f87
Storm Shadow <83181647+StormShadow058@users.noreply.github.com>
2022-01-20 17:21:20 +0530

    Initial Commit

---
+++ ColPicky/ColPickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColPickyApp.swift
+//  ColPicky
+//
+//  Created by Vansh Maheshwari on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ColPickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColPicky/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColPicky
+//
+//  Created by Vansh Maheshwari on 20/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
